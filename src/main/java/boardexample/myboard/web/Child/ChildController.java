9aeac5389984e7e961ae7b1e550c0c1e8c5c7949
7gnsgml7@kakao.com <7gnsgml7@kakao.com>
2023-02-02 16:29:17 +0900

    자식 백신 생성 페이지 추가

---
+++ src/main/java/boardexample/myboard/web/Child/ChildController.java
@@ -0,0 +1,51 @@
+package boardexample.myboard.web.Child;
+
+import boardexample.myboard.domain.user.User;
+import boardexample.myboard.web.session.SessionConst;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/children")
+public class ChildController {
+    private final ChildService childService;
+
+    @GetMapping
+    public String childListPage(Model model, HttpServletRequest request){
+        User user = getUser(request);
+        List<ChildResponse> child = childService.findByUserAllChild(user.getId());
+        model.addAttribute("child", child);
+        return "children";
+    }
+
+    @GetMapping("/add")
+    public String childAddPage(){
+        return "childrenAdd";
+    }
+
+
+    @PostMapping("/add")
+    public String childAdd(ChildRequest childRequest ,HttpServletRequest request){
+        User user = getUser(request);
+        childService.save(user.getId(), childRequest);
+        return "children";
+    }
+
+
+
+    private User getUser(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(SessionConst.LOGIN_USER);
+        return user;
+    }
+}
